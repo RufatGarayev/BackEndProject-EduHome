19fb2e78874a889992a56b7329778e92e4105872
Your Name <you@example.com>
2021-01-03 19:26:35 +0400

    CRUD_start

--- EduHome/Areas/Admin/Controllers/CoursesController.cs
+++ EduHome/Areas/Admin/Controllers/CoursesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EduHome.DAL;
+using EduHome.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EduHome.Areas.Admin.Controllers
@@ -17,7 +18,35 @@ namespace EduHome.Areas.Admin.Controllers
         }
         public IActionResult Index()
         {
+            return View(_context.CoursesWeOffers.Where(cwo => cwo.IsDeleted==false).ToList());
+        }
+
+
+        //Create - Get metodu
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        //Create - Post metodu
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CoursesWeOffer course)
+        {
+            if (!ModelState.IsValid) return NotFound(course);
+
+            bool isExist = _context.CoursesWeOffers.Where(k => k.IsDeleted == false)
+                .Any(k => k.Name.ToLower() == course.Name.ToLower());
+            if (isExist)
+            {
+                ModelState.AddModelError("Name", "You have already written something same this one!");
+                return View();
+            }
+
+            course.IsDeleted = false;
+            await _context.CoursesWeOffers.AddAsync(course);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- EduHome/ViewComponents/HomeHeaderViewComponent.cs
+++ EduHome/ViewComponents/HomeHeaderViewComponent.cs
@@ -1,5 +1,6 @@
 ﻿using EduHome.DAL;
 using EduHome.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,13 +12,22 @@ namespace EduHome.ViewComponents
     public class HomeHeaderViewComponent:ViewComponent
     {
         private readonly AppDbContext _context;
-        public HomeHeaderViewComponent(AppDbContext context)
+        private readonly UserManager<AppUser> _userManager;
+        public HomeHeaderViewComponent(AppDbContext context, UserManager<AppUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         public async Task<IViewComponentResult> InvokeAsync()
         {
+            ViewBag.UserFullname = string.Empty;
+            if (User.Identity.IsAuthenticated)
+            {
+                string username = (await _userManager.FindByNameAsync(User.Identity.Name)).UserName;
+                ViewBag.UserName = username;
+            }
+
             Bio model = _context.Bios.FirstOrDefault();
             return View(await Task.FromResult(model));
         }
