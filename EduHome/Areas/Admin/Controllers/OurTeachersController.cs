7dd13512d5ae67b69c1d2baeece2f9c836e2e44b
Your Name <you@example.com>
2021-01-04 12:08:04 +0400

    admin_Teachers_created

---
+++ EduHome/Areas/Admin/Controllers/OurTeachersController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EduHome.DAL;
+using EduHome.Models;
+
+namespace EduHome.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OurTeachersController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public OurTeachersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/OurTeachers
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.OurTeachers.ToListAsync());
+        }
+
+        // GET: Admin/OurTeachers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ourTeacher = await _context.OurTeachers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (ourTeacher == null)
+            {
+                return NotFound();
+            }
+
+            return View(ourTeacher);
+        }
+
+        // GET: Admin/OurTeachers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/OurTeachers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Image,Name,Position,Facebook,Pinterest,Vimeo,Twitter,IsDeleted,TimeDeleted")] OurTeacher ourTeacher)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(ourTeacher);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(ourTeacher);
+        }
+
+        // GET: Admin/OurTeachers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ourTeacher = await _context.OurTeachers.FindAsync(id);
+            if (ourTeacher == null)
+            {
+                return NotFound();
+            }
+            return View(ourTeacher);
+        }
+
+        // POST: Admin/OurTeachers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Image,Name,Position,Facebook,Pinterest,Vimeo,Twitter,IsDeleted,TimeDeleted")] OurTeacher ourTeacher)
+        {
+            if (id != ourTeacher.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(ourTeacher);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OurTeacherExists(ourTeacher.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(ourTeacher);
+        }
+
+        // GET: Admin/OurTeachers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ourTeacher = await _context.OurTeachers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (ourTeacher == null)
+            {
+                return NotFound();
+            }
+
+            return View(ourTeacher);
+        }
+
+        // POST: Admin/OurTeachers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var ourTeacher = await _context.OurTeachers.FindAsync(id);
+            _context.OurTeachers.Remove(ourTeacher);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OurTeacherExists(int id)
+        {
+            return _context.OurTeachers.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ EduHome/Areas/Admin/Controllers/WelcomeToEduHomesController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EduHome.DAL;
+using EduHome.Models;
+
+namespace EduHome.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class WelcomeToEduHomesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public WelcomeToEduHomesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/WelcomeToEduHomes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.WelcomeToEduHomes.ToListAsync());
+        }
+
+        // GET: Admin/WelcomeToEduHomes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var welcomeToEduHome = await _context.WelcomeToEduHomes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (welcomeToEduHome == null)
+            {
+                return NotFound();
+            }
+
+            return View(welcomeToEduHome);
+        }
+
+        // GET: Admin/WelcomeToEduHomes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/WelcomeToEduHomes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Image,Title,Description,IsDeleted,TimeDeleted")] WelcomeToEduHome welcomeToEduHome)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(welcomeToEduHome);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(welcomeToEduHome);
+        }
+
+        // GET: Admin/WelcomeToEduHomes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var welcomeToEduHome = await _context.WelcomeToEduHomes.FindAsync(id);
+            if (welcomeToEduHome == null)
+            {
+                return NotFound();
+            }
+            return View(welcomeToEduHome);
+        }
+
+        // POST: Admin/WelcomeToEduHomes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Image,Title,Description,IsDeleted,TimeDeleted")] WelcomeToEduHome welcomeToEduHome)
+        {
+            if (id != welcomeToEduHome.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(welcomeToEduHome);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!WelcomeToEduHomeExists(welcomeToEduHome.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(welcomeToEduHome);
+        }
+
+        // GET: Admin/WelcomeToEduHomes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var welcomeToEduHome = await _context.WelcomeToEduHomes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (welcomeToEduHome == null)
+            {
+                return NotFound();
+            }
+
+            return View(welcomeToEduHome);
+        }
+
+        // POST: Admin/WelcomeToEduHomes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var welcomeToEduHome = await _context.WelcomeToEduHomes.FindAsync(id);
+            _context.WelcomeToEduHomes.Remove(welcomeToEduHome);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool WelcomeToEduHomeExists(int id)
+        {
+            return _context.WelcomeToEduHomes.Any(e => e.Id == id);
+        }
+    }
+}
