77d0fca72fbec6d59a6340d1ae912034aea27276
Your Name <you@example.com>
2021-01-05 02:21:56 +0400

    admin_Workshops_created

---
+++ EduHome/Areas/Admin/Controllers/WorkShopsController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EduHome.DAL;
+using EduHome.Models;
+
+namespace EduHome.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class WorkShopsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public WorkShopsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/WorkShops
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.WorkShops.Include(w => w.Event);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // GET: Admin/WorkShops/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var workShop = await _context.WorkShops
+                .Include(w => w.Event)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (workShop == null)
+            {
+                return NotFound();
+            }
+
+            return View(workShop);
+        }
+
+        // GET: Admin/WorkShops/Create
+        public IActionResult Create()
+        {
+            ViewData["EventId"] = new SelectList(_context.Events, "Id", "Image");
+            return View();
+        }
+
+        // POST: Admin/WorkShops/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Description,IsDeleted,TimeDeleted,EventId")] WorkShop workShop)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(workShop);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EventId"] = new SelectList(_context.Events, "Id", "Image", workShop.EventId);
+            return View(workShop);
+        }
+
+        // GET: Admin/WorkShops/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var workShop = await _context.WorkShops.FindAsync(id);
+            if (workShop == null)
+            {
+                return NotFound();
+            }
+            ViewData["EventId"] = new SelectList(_context.Events, "Id", "Image", workShop.EventId);
+            return View(workShop);
+        }
+
+        // POST: Admin/WorkShops/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Description,IsDeleted,TimeDeleted,EventId")] WorkShop workShop)
+        {
+            if (id != workShop.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(workShop);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!WorkShopExists(workShop.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EventId"] = new SelectList(_context.Events, "Id", "Image", workShop.EventId);
+            return View(workShop);
+        }
+
+        // GET: Admin/WorkShops/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var workShop = await _context.WorkShops
+                .Include(w => w.Event)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (workShop == null)
+            {
+                return NotFound();
+            }
+
+            return View(workShop);
+        }
+
+        // POST: Admin/WorkShops/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var workShop = await _context.WorkShops.FindAsync(id);
+            _context.WorkShops.Remove(workShop);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool WorkShopExists(int id)
+        {
+            return _context.WorkShops.Any(e => e.Id == id);
+        }
+    }
+}
